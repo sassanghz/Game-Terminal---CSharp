f2e29b9f18bb94ccc760f757c62ddbb648aa1267
Sassan Ghazi <117306645+sassanghz@users.noreply.github.com>
2024-09-07 23:48:52 -0400

    Life Game: C#

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using MyApplication;
+using System;
+
+namespace Games
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VideoGame game = new VideoGame();
+            //Console.WriteLine(game.name);
+
+            VideoGame.intro();
+        }
+    }
+}
